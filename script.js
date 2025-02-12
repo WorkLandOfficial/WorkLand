2d86ca9d667007205df6e41f40e2350564586ea2
WorkLand <worklandoffical@gmail.com>
2025-02-12 22:41:16 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.location.href.includes("ip.html")) {
+        fetch("https://super-heavily-krill.ngrok-free.app", {method: 'GET', redirect: 'follow'}).then((response) => {
+            if (response.ok) {
+                document.getElementById("status").innerText = "ВКЛЮЧЕН"
+                document.getElementById("status").style = "color: green;"
+            }
+
+            else {
+                document.getElementById("status").innerText = "ВЫКЛЮЧЕН"
+                document.getElementById("status").style = "color: red;"
+            }
+        }).catch((error) => {
+            if (error) {
+                document.getElementById("status").innerText = "ВЫКЛЮЧЕН"
+                document.getElementById("status").style = "color: red;"
+            }
+        })
+    }
+});
+
 function copy()
 {
     var element = document.getElementById("ip");
